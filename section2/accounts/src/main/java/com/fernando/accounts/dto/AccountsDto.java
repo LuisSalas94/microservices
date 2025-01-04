ba73764437b7919267aff6271df9e997e6e3645e
Fernando Salas <luisfernandosalasg@gmail.com>
2025-01-04 00:50:34 -0500

    first commit

---
+++ section2/accounts/src/main/java/com/fernando/accounts/dto/AccountsDto.java
@@ -0,0 +1,36 @@
+package com.fernando.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
+public class AccountsDto {
+
+    @NotEmpty(message = "AccountNumber cannot be null or empty")
+    @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number must be 10 digits")
+    @Schema(
+            description = "Account Number of Fernando Salas account",
+            example = "123456789"
+    )
+    private Long accountNumber;
+
+    @NotEmpty(message = "AccountType cannot be null or empty")
+    @Schema(
+            description = "Account type of Fernando Salas",
+            example = "Savings"
+    )
+    private String accountType;
+
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
+    @Schema(
+            description = "Fernando Salas branch address",
+            example = "123 New York"
+    )
+    private String branchAddress;
+}
---
+++ section4/loans/src/main/java/com/fernando/loans/LoansApplication.java
@@ -0,0 +1,40 @@
+package com.fernando.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Fernando Salas Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Fernando Salas",
+                        email = "luis@gmail.com",
+                        url = "www.google.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "www.google.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Fernando Salas Loans microservice REST API Documentation",
+                url = "www.google.com"
+        )
+)
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
